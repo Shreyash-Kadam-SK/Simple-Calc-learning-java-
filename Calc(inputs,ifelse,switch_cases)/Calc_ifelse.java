17b9412dfbf39aeed0b46bfdee715a8426d44a2b
Shreyash-Kadam-SK <kadam.shreyash.d@gmail.com>
2024-09-21 22:20:32 +0530

    name was wrong

--- Calc(inputs,ifelse,switch_cases)/Calc_ifelse.java
+++ Calc(inputs,ifelse,switch_cases)/Calc_ifelse.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Calc_Ifelse
+public class Calc_ifelse
   {
     public static void main (String[]args)
       {
@@ -8,11 +8,13 @@ public class Calc_Ifelse
 
         System.out.print("Enter the First Number:");
         Double num1 = input.nextDouble();
+        input.nextLine();
         System.out.print("Enter your second number:");
         Double num2 = input.nextDouble();
+        input.nextLine();
         System.out.print("What operation do you want to do? :");
         String operation = input.nextLine();
-
+      
         if (operation.equals("sum"))
         {
           System.out.printf("%f + %f = %f",num1,num2,num1+num2);
