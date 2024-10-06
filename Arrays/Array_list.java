a089f9fd61564fb23f482b5e06651cd584f2b825
Shreyash-Kadam-SK <kadam.shreyash.d@gmail.com>
2024-10-06 14:56:10 +0530

    loops code sompleted

---
+++ Arrays/Array_list.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList.*;
+
+class Array_list {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ While loop/While_loop.java
@@ -0,0 +1,13 @@
+
+class While_loop {
+    public static void main(String args[])
+    {
+        int number = 5;
+        int multiplier = 1;
+        while (multiplier<=10)
+        {
+            System.out.printf("%d X %d = %d\n",number,multiplier,number*multiplier);
+            multiplier++;
+        }
+    }
+}
